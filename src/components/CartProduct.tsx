f08ecab7349eaab50ace1b6ddac077befa026f78
aga87 <aga.labonarska@outlook.com>
2023-05-16 12:23:09 +0200

    Improve cart design

--- src/components/CartProduct.tsx
+++ src/components/CartProduct.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import { useAppDispatch } from '../redux/typed-hooks';
-import { removeOneFromCart } from '../redux/reducers/cartProductsSlice';
+import {
+  addToCart,
+  removeOneFromCart
+} from '../redux/reducers/cartProductsSlice';
 
 type CartProductProps = {
   cartProduct: CartProduct;
@@ -10,20 +14,43 @@ type CartProductProps = {
 export const CartProduct = ({ cartProduct }: CartProductProps) => {
   const dispatch = useAppDispatch();
 
+  const handleAddToCart = () => {
+    const { quantity, ...product } = cartProduct;
+    dispatch(addToCart(product));
+  };
+
   const handleRemoveOneFromCart = () => {
     dispatch(removeOneFromCart(cartProduct.id));
   };
 
   const { name, price, quantity } = cartProduct;
+
+  const totalCost = `€${(quantity * price).toFixed(2)}`;
+
   return (
-    <>
-      <h2>{name}</h2>
-      <p>{quantity} total</p>
-      <p>${(quantity * price).toFixed(2)}</p>
-      <Button size='sm' onClick={handleRemoveOneFromCart}>
-        Remove
-      </Button>
-      <hr />
-    </>
+    <Card>
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>Total {totalCost}</Card.Text>
+        <Card.Text>Quantity</Card.Text>
+        <div>
+          <Button
+            size='sm'
+            onClick={handleRemoveOneFromCart}
+            style={{ marginRight: '10px' }}
+          >
+            -
+          </Button>
+          {quantity}
+          <Button
+            size='sm'
+            onClick={handleAddToCart}
+            style={{ marginLeft: '10px' }}
+          >
+            +
+          </Button>
+        </div>
+      </Card.Body>
+    </Card>
   );
 };
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -15,31 +15,36 @@ export const Cart = () => {
   const totalCostOfCartProducts = useAppSelector(selectTotalCostOfCartProducts);
 
   return (
-    <>
+    <div style={{ paddingBottom: '20px' }}>
       <h1 className='p-3'>Cart</h1>
       {cartProductsQuantity > 0 ? (
         <>
           <p>Items in your cart:</p>
           {cartProducts.map(cartProduct => (
-            <React.Fragment key={cartProduct.id}>
+            <div
+              key={cartProduct.id}
+              style={{
+                maxWidth: '400px',
+                textAlign: 'center',
+                marginBottom: '20px'
+              }}
+            >
               <CartProduct cartProduct={cartProduct} />
-            </React.Fragment>
+            </div>
           ))}
-
-          <p>
+          <p style={{ fontSize: '20px' }}>
             <b>Total: {totalCostOfCartProducts}</b>
           </p>
-
           <span style={{ marginRight: '10px' }}>
             <Button variant='success'>Purchase items!</Button>
           </span>
         </>
       ) : (
-        <p>There are no items in your cart!</p>
+        <p>There are no items in your cart.</p>
       )}
       <NavLink to='/' className='btn btn-primary'>
         Back to the Store
       </NavLink>
-    </>
+    </div>
   );
 };
