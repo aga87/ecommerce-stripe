2e1417cecd659593c59e5de536dfc9e0e2972e8f
aga87 <aga.labonarska@outlook.com>
2023-05-18 14:18:30 +0200

    Set cors: allow cross-origin API requests from the client

--- server/src/startup/routes.ts
+++ server/src/startup/routes.ts
@@ -1,6 +1,14 @@
 import { Application } from 'express';
+import cors from 'cors';
 import { ping } from '../routes/ping';
 
 export const routes = (app: Application) => {
+  const { CLIENT_URL } = process.env;
+  const host = CLIENT_URL || 'http://localhost:3000';
+  app.use(
+    cors({
+      origin: host
+    })
+  );
   app.use('/api/v1/ping', ping);
 };
